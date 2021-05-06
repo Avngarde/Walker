20a18a3963dec20f4695f423424ca45846045fea
Kamil Paczkowski <pivotguy888@gmail.com>
2021-05-06 14:05:10 +0200

    I was bored at my job

---
+++ Walker/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace GwinstekLCRController
+{
+    class Program
+    {
+        static void printGrid(char[,] grid)
+        {
+            Console.Clear();
+            for (int y = 0; y <= 4; y++)
+            {
+                for (int x = 0; x <= 4; x++)
+                {
+                    Console.Write(grid[x, y]);
+                }
+
+                Console.WriteLine();
+            } 
+        }
+
+        static char[,] redrawGrid(char[,] grid, int currentX, int currentY, char changedParam, string ascOrDesc)
+        {
+            grid[currentX, currentY] = '.';
+
+            switch (changedParam)
+            {
+                case 'x':
+                    if (ascOrDesc == "asc")
+                    {
+                        grid[currentX + 1, currentY] = '@';
+                    }
+
+                    if (ascOrDesc == "desc")
+                    {
+                        grid[currentX - 1, currentY] = '@';
+                    }
+                    break;
+
+                case 'y':
+                    if (ascOrDesc == "asc")
+                    {
+                        grid[currentX, currentY + 1] = '@';
+                    }
+
+                    if (ascOrDesc == "desc")
+                    {
+                        grid[currentX, currentY - 1] = '@';
+                    }
+                    break;
+            }
+
+
+            return grid;
+        }
+
+        static void Main()
+        {
+            char[,] grid = new char[5, 5] {
+                {'@', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.'},
+            };
+            int playerX = 0;
+            int playerY = 0;
+
+            while (true)
+            {
+                printGrid(grid);
+                Console.WriteLine(playerX + " " + playerY);
+
+                ConsoleKeyInfo newMovement = Console.ReadKey(true);
+                switch (newMovement.KeyChar)
+                {
+                    case 'w':
+                        if (playerY > 0)
+                        {
+                            grid = redrawGrid(grid, playerX, playerY, 'y', "desc");
+                            playerY -= 1;
+                        }
+                        break;
+
+                    case 'a':
+                        if (playerX > 0)
+                        {
+                            grid = redrawGrid(grid, playerX, playerY, 'x', "desc");
+                            playerX -= 1;
+                        }
+                        break;
+
+                    case 's':
+                        if (playerY < 4)
+                        {
+                            grid = redrawGrid(grid, playerX, playerY, 'y', "asc");
+                            playerY += 1;
+                        }
+                        break;
+
+                    case 'd':
+                        if (playerX < 4)
+                        {
+                            grid = redrawGrid(grid, playerX, playerY, 'x', "asc");
+                            playerX += 1;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+} 
